0bd460c93d6058302e288d6fb895f4867dce95cc
Aleem Haji <hajial@gmail.com>
2015-08-03 00:36:00 -0400

    Super basic set up with minimal tests and coverage.

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,54 @@
 "use strict";
 
-var code = require("..");
+var request = require("request");
+var mongoose = require("mongoose");
+var ts = require("./testServer");
+
+var server = ts.server;
+var Entity = ts.Entity;
 
 module.exports = {
-    "Application Start": {
-        "Throws Exception": function(test) {
-            test.expect(1);
-            test.throws(function() {
-                code();
+    setUp: function(done) {
+        mongoose.connect("mongodb://localhost:27017");
+        mongoose.connection.once("open", function() {
+            server.listen(8080, function() {
+                done();
             });
+        });
+    },
+    tearDown: function(done) {
+        mongoose.connection.close(function() {
+            server.close(function() {
+                done();
+            });
+        });
+    },
+    "Succeeds on non-running route": function(test) {
+        test.expect(3)
+        request({uri: "http://localhost:8080/fetch", method: "GET"}, function(err, resp, body) {
+            if ( typeof body === "string" && body.length > 0 ) { // Request is a finicky package.
+                body = JSON.parse(body);
+            }
+            test.ifError(err);
+            test.equal(resp.statusCode, 200);
+            test.deepEqual(body, {});
             test.done();
-        }
+        });
+    },
+    "Fetches entity from route": function(test) {
+        var obj = new Entity({name: "abc"});
+        obj.save(function(err) {
+            test.ifError(err);
+            request({uri: "http://localhost:8080/fetch/" + obj.id, method: "GET"}, function(err, resp, body) {
+                if ( typeof body === "string" && body.length > 0 ) { // Request is a finicky package.
+                    body = JSON.parse(body);
+                }
+                test.ifError(err);
+                test.equal(resp.statusCode, 200);
+                test.deepEqual(body.name, obj.name);
+                test.deepEqual(body._id, obj.id);
+                test.done();
+            });
+        });
     }
 };

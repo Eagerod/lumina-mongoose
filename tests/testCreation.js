64353c7ad2228ec3be6b11a2395c97bb39e60e27
Aleem Haji <hajial@gmail.com>
2016-07-27 18:51:19 -0400

    Added in fetch from query components, but poorly tested.

--- tests/testCreation.js
+++ tests/testCreation.js
@@ -3,11 +3,24 @@
 var lumen = require("./configureServer").lumen;
 
 module.exports = {
-    "Poorly constructed lumina function": function(test) {
+    "Poorly constructed lumina fetchObjectsFromRoute function": function(test) {
         test.expect(1);
         test.throws(function() {
             lumen.illuminate({
                 fetchObjectsFromRoute: {},
+                handler: function(req, res, next) {
+                    res.send(200);
+                    return next();
+                }
+            });
+        });
+        test.done();
+    },
+    "Poorly constructed lumina fetchObjectsFromQuery function": function(test) {
+        test.expect(1);
+        test.throws(function() {
+            lumen.illuminate({
+                fetchObjectsFromQuery: {},
                 handler: function(req, res, next) {
                     res.send(200);
                     return next();
